da3429352877e65d1394ddfa08df59215f3f3555
BILLthebuilder <billkariri@gmail.com>
2018-09-04 12:38:29 +0300

    feat: Add event listener that enables flipping of cards

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +24,18 @@ function shuffle(array) {
 
     return array;
 }
+
+const deck = document.querySelector('.deck');
+
+	deck.addEventListener('click', event => {
+		const clicked = event.target;
+		if(clicked.classList.contains('card')){
+			clicked.classList.toggle('open');
+			clicked.classList.toggle('show');
+		}
+	});
+
+
 
 
 /*
